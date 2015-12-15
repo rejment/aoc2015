94169d0d70290417eb1782cc52d76d6f7d669e09
Daniel Rejment <daniel.rejment@tacton.com>
2015-12-15 06:55:59 +0100

    Remove json lib from day 12

--- src/Day12.java
+++ src/Day12.java
@@ -1,28 +1,33 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author daniel.rejment@tacton.com (2015-12-12)
  */
 public class Day12 {
     public static void main(String[] args) throws IOException {
-        System.out.println(sum(JSONObject.parse(new String(Files.readAllBytes(Paths.get("src/day12.txt"))))));
+        Pattern obj = Pattern.compile("\\{[^{}]*\\}");
+        String input = new String(Files.readAllBytes(Paths.get("src/day12.txt")));
+        System.out.println(getSum(input));
+
+        while (input.contains("{")) {
+            Matcher m = obj.matcher(input);
+            if (m.find()) {
+                input = input.replace(m.group(), m.group().contains(":\"red\"") ? "0" : Integer.toString(getSum(m.group())));
+            }
+        }
+        System.out.println(getSum(input));
     }
 
-    private static int sum(JSONObject o) {
-        if (o.isNumber()) {
-            return o.asInteger();
-        } else if (o.isList()) {
-            return o.values().stream().mapToInt(Day12::sum).sum();
-        } else if (o.isObject()) {
-            if (o.entrySet().stream().map(Map.Entry::getValue).map(JSONObject::asString).anyMatch(n -> n.equals("red"))) {
-                return 0;
-            }
-            return o.entrySet().stream().map(Map.Entry::getValue).mapToInt(Day12::sum).sum();
-        } else {
-            return 0;
+    private static int getSum(String input) {
+        Matcher matcher = Pattern.compile("-?\\d+").matcher(input);
+        int sum = 0;
+        while (matcher.find()) {
+            sum += Integer.parseInt(matcher.group(0));
         }
+        return sum;
     }
 }
